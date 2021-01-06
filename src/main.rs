8078d6733bf6fb8839235c5681afd4965f4a6985
Babis Chalios <mail@bchalios.io>
2021-01-06 10:04:40 -0600

    Working version of web-classify
    Fix the arguments for the vaccel calls and return the result of the
    operation in the body of the response. Also, make the server listen for
    requests to all of the host's addresses.
    
    Signed-off-by: Babis Chalios <mail@bchalios.io>

--- src/main.rs
+++ src/main.rs
@@ -24,19 +24,26 @@ async fn classify_image(
         return Err(error::ErrorBadRequest("Could not create vaccel session"));
     }
 
-    let text = String::with_capacity(512);
-    let imagepath = String::with_capacity(512);
+    let mut text = vec![0;512];
+    let mut imagepath = vec![0;512];
 
     if vaccel_bindings::image_classification(
         &mut sess,
         &mut body,
-        &mut text.into_bytes(),
-        &mut imagepath.into_bytes(),
+        &mut text,
+        &mut imagepath,
     ).is_err() {
         return Err(error::ErrorBadRequest("Classification operations failed"));
     }
 
-    Ok(HttpResponse::Ok().body("Yoopee"))
+    if vaccel_bindings::close_session(&mut sess).is_err() {
+        return Err(error::ErrorBadRequest("Could not close vaccel session"));
+    }
+
+    let mut tags = String::from_utf8(text).unwrap();
+    tags.retain(|c| c != '\0');
+    println!("test: {:?}", tags);
+    Ok(HttpResponse::Ok().body(tags))
 }
 
 #[actix_web::main]
@@ -45,7 +52,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(classify_image)
     })
-    .bind("127.0.0.1:3030")?
+    .bind("0.0.0.0:3030")?
         .run()
         .await
 }
