e95f0113c75e814344583727210c9608c87effe2
Babis Chalios <mail@bchalios.io>
2021-04-19 17:31:48 +0200

    Fix vAccel API
    1. Change the crate to point to the cloudkernels repo which is the
    maintained one.
    2. Fix calls to the vaccelrt to use the new rust bindings API
    3. Cleanup a bit error handling
    
    Signed-off-by: Babis Chalios <mail@bchalios.io>

--- src/main.rs
+++ src/main.rs
@@ -1,91 +1,110 @@
-use actix_web::{post, App, web, HttpResponse, HttpServer, Error, error};
+use actix_web::{
+    dev::HttpResponseBuilder, error, http::header, http::StatusCode, post, web, App, HttpResponse,
+    HttpServer,
+};
+use derive_more::{Display, Error};
 use futures::StreamExt;
-use log::{debug, error};
+use log::debug;
+use vaccel_bindings::vaccel_session;
 
 extern crate pretty_env_logger;
 
 // maximum payload is 256M
 const MAX_SIZE: usize = 268_435_456;
 
+#[derive(Debug, Display, Error)]
+enum Error {
+    #[display(fmt = "vAccel error({}): {}", err, msg)]
+    VAccel { err: u32, msg: String },
+
+    #[display(fmt = "An internal error occured: {}", msg)]
+    Internal { msg: String },
+
+    #[display(fmt = "Request error: {}", msg)]
+    Request { msg: String },
+}
+
+impl error::ResponseError for Error {
+    fn error_response(&self) -> HttpResponse {
+        HttpResponseBuilder::new(self.status_code())
+            .set_header(header::CONTENT_TYPE, "text/html; charset=utf-8")
+            .body(self.to_string())
+    }
+    fn status_code(&self) -> StatusCode {
+        match *self {
+            Error::VAccel { .. } => StatusCode::BAD_REQUEST,
+            Error::Internal { .. } => StatusCode::BAD_REQUEST,
+            Error::Request { .. } => StatusCode::BAD_REQUEST,
+        }
+    }
+}
+
 #[post("/classify")]
-async fn classify_image(
-    mut payload: web::Payload
-) -> Result<HttpResponse, Error> {
+async fn classify_image(mut payload: web::Payload) -> Result<HttpResponse, Error> {
     debug!("Handling classification request");
 
     let mut body = web::BytesMut::new();
 
-    let hostname =
-        match hostname::get() {
-            Ok(name) => name,
-            Err(_) => {
-                error!("Could not retrieve server's hostname");
-                return Err(error::ErrorBadRequest("Server error: Could not get hostname"));
-            }
-        };
+    let hostname = hostname::get().map_err(|_e| Error::Internal {
+        msg: "Server error: Could not get hostname".to_string(),
+    })?;
 
     while let Some(chunk) = payload.next().await {
-        let chunk = chunk?;
+        let chunk = chunk.map_err(|_e| Error::Request {
+            msg: "Could not read payload".to_string(),
+        })?;
 
         if (body.len() + chunk.len()) > MAX_SIZE {
-            return Err(error::ErrorBadRequest("overflow"));
+            return Err(Error::Request {
+                msg: "overflow".to_string(),
+            });
         }
         body.extend_from_slice(&chunk);
     }
     debug!("Received image");
 
-    let mut sess: vaccel_bindings::vaccel_session = Default::default();
-    if vaccel_bindings::new_session(&mut sess, 0).is_err() {
-        return Err(error::ErrorBadRequest("Could not create vaccel session"));
-    }
+    let mut sess = vaccel_session::new(0).map_err(|e| Error::VAccel {
+        err: e,
+        msg: "Could not create vAccel session".to_string(),
+    })?;
+
     debug!("Created vaccel session with id: {:?}", sess.session_id);
 
-    let mut text = vec![0;512];
-    let mut imagepath = vec![0;512];
+    let (tags, _imgpath) = sess
+        .image_classification(&body)
+        .map_err(|e| Error::VAccel {
+            err: e,
+            msg: "Could not classify image".to_string(),
+        })?;
 
-    let tags = match vaccel_bindings::image_classification(
-        &mut sess,
-        &mut body,
-        &mut text,
-        &mut imagepath,
-    ) {
-        Ok(()) => {
-            let mut t =
-                match String::from_utf8(text) {
-                    Ok(t) => t,
-                    Err(_e) => return Err(error::ErrorBadRequest("Could not get vaccel string")),
-                };
-
-            t.retain(|c| c != '\0');
-            t
-        }
+    let tags = match String::from_utf8(tags) {
+        Ok(mut t) => t.retain(|c| c != '\0'),
         Err(_e) => {
-            return Err(error::ErrorBadRequest("Classification operations failed"));
+            return Err(Error::Internal {
+                msg: "Could not get tags".to_string(),
+            })
         }
     };
 
     debug!("Classification completed. Tags: {:?}", tags);
 
-    if vaccel_bindings::close_session(&mut sess).is_err() {
-        error!("Could not close vaccel session: {:?}", sess.session_id);
-        return Err(error::ErrorBadRequest("Could not close vaccel session"));
-    }
+    sess.close().map_err(|e| Error::VAccel {
+        err: e,
+        msg: "Could not close vAccel session".to_string(),
+    })?;
+
     debug!("Closed vaccel session {:?}", sess.session_id);
 
-    Ok(HttpResponse::Ok()
-       .body(format!("[{:?}]: {:?}\n", hostname, tags)))
+    Ok(HttpResponse::Ok().body(format!("[{:?}]: {:?}\n", hostname, tags)))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     pretty_env_logger::init_timed();
 
-    HttpServer::new(|| {
-        App::new()
-            .service(classify_image)
-    })
-    .workers(1)
-    .bind("0.0.0.0:3030")?
+    HttpServer::new(|| App::new().service(classify_image))
+        .workers(1)
+        .bind("0.0.0.0:3030")?
         .run()
         .await
 }
